59c3f0d19e60cb7b426b886b58990bf37fa2ce31
diosMantequilla <yagel.salazar.dev@gmail.com>
2024-08-29 16:14:38 -0600

    clase jueves

--- main.js
+++ main.js
@@ -17,11 +17,16 @@ document.addEventListener('DOMContentLoaded', () => {
             nombre: 'panditas',
             precio: 18,
             imagen: 'panditas.jpg'
+        }, {
+            id: 4,
+            nombre: 'panditas enchilados',
+            precio: 18,
+            imagen: 'panditas.jpg'
         }
     ]
 
     let carrito = []
-    const divisa = 'pesos'
+    const divisa = 'pesos' // ignoramos un ratito
     // traer elementos del DOM
     const DOMitems = document.querySelector('#items')
     const DOMcarrito = document.querySelector('#carrito')
@@ -30,11 +35,45 @@ document.addEventListener('DOMContentLoaded', () => {
     // funcionalidades
     function renderizarProductos() {
         baseDeDatos.forEach( function (producto) {
-            
+            // dar ordenes de que hacer cuando vaya en cada producto
+
+            // vamos a crear el contenedor card
+            const miNodo = document.createElement('div')
+            miNodo.classList.add('card', 'col-sm-4')
+            // vamos a crear nuestra imagen
+            const miNodoImagen = document.createElement('img')
+            miNodoImagen.classList.add('card-img-top')
+            miNodoImagen.setAttribute('src', producto.imagen)
+            // creamos el card body
+            const miNodoCardBody = document.createElement('div')
+            miNodoCardBody.classList.add('card-body')
+            // cramos el titulo en un h5
+            const miNodoTitulo = document.createElement('h5')
+            miNodoTitulo.classList.add('card-title')
+            miNodoTitulo.textContent = producto.nombre
+            // creamos un parrafo para poner el precio
+            const miNodoPrecio = document.createElement('p')
+            miNodoPrecio.classList.add('card-text')
+            miNodoPrecio.textContent = producto.precio
+            // creamos un boton para poder agregar los elementos al carrito
+            const miNodoBoton = document.createElement('button')
+            miNodoBoton.classList.add('btn', 'btn-primary')
+            miNodoBoton.textContent = '+'
+
+            // unimos todos los elementos que creamos
+            miNodoCardBody.appendChild(miNodoTitulo)
+            miNodoCardBody.appendChild(miNodoPrecio)
+            miNodoCardBody.appendChild(miNodoBoton)
+            miNodo.appendChild(miNodoImagen)
+            miNodo.appendChild(miNodoCardBody)
+
+            // insertamos nuestra card dentro del main con el id items
+            DOMitems.appendChild(miNodo)
         })
 
     }
 
 
+    renderizarProductos()
     // aqui termina el codigo
 })
